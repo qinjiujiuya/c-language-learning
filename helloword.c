d0f26c2cdcb0e77fe0ea5ba3505ccb02a50f7da3
qinjiujiuya <1210624961@qq.com>
2024-10-07 16:54:15 +0800

    first message

---
+++ helloword.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("hello word!\n");
+    printf("%ld\n", sizeof(int));
+    printf("%ld\n", sizeof(float));
+    printf("%ld\n", sizeof(double));
+    printf("%ld\n", sizeof(long));
+    printf("%ld\n", sizeof(long long));
+    return 0;
+}
